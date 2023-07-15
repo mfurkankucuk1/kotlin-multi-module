946a78cb201759ae373cc426f061ca25546c9cb7
furkan5443905 <furkan@eywin.com.tr>
2023-07-15 18:17:57 +0300

    first commit

---
+++ app/src/main/java/tr/mfk/multimodule/MainActivity.kt
@@ -0,0 +1,11 @@
+package tr.mfk.multimodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
